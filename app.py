b00229d1b84f1fbd581050fde2293db4348c3e47
Jean <jean.sls.snts@gmail.com>
2025-07-21 22:05:04 -0300

    [feat] - Tela de login inicial validada

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 #importando a lib
 import customtkinter as ctk
 
+#As funções do aplicativo devem ser criadas antes da interface
+def validar_login():
+    usuario = campo_usuario.get()
+    senha = campo_senha.get()
+    if usuario=='teste' and senha=='123':
+        print('sucesso')
+        resultado_login.configure(text='Login feito com sucesso!',
+                                  text_color='green')
+    else:
+        resultado_login.configure(text='Login incorreto',
+                                  text_color='red')
+
 #configuração de aparência
 ctk.set_appearance_mode('dark')
 
@@ -10,9 +22,20 @@ app.title('Sistema de gerenciamento PC')
 app.geometry('400x400')
 
 #Criação dos campos
-campo_usuario = ctk.CTkLabel(app,text='Usuario')
-campo_senha = ctk.CTkLabel(app,text='Senha')
+label_usuario = ctk.CTkLabel(app,text='Usuario')
+label_usuario.pack(pady=5)
+campo_usuario = ctk.CTkEntry(app, placeholder_text="Digite o seu usuário...")
 campo_usuario.pack(pady=5)
+
+label_senha = ctk.CTkLabel(app,text='Senha')
+label_senha.pack(pady=5)
+campo_senha = ctk.CTkEntry(app, placeholder_text="Digite sua senha...", show="*")
+campo_senha.pack(pady=5)
+
+botao = ctk.CTkButton(app, text='Login', command=validar_login)
+botao.pack(pady=10)
+resultado_login = ctk.CTkLabel(app,text='')
+resultado_login.pack(pady=10)
 #check_lembrar = ctk.CHECKBUTTON(app)
 #Criação das funções de funcionalidades
 #inicia o loop da aplicação
